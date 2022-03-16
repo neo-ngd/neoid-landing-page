032e0978366f215188da564b501ba80f29776f54
liuqiang <liuqiang1357@163.com>
2022-03-16 15:19:22 +0800

    update project config

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,30 +8,16 @@ const foldersUnderSrc = fs
 module.exports = {
 	root: true,
 	env: {
+		es6: true,
 		node: true,
 		jest: true,
 	},
-	extends: [
-		'eslint:recommended',
-		'plugin:@typescript-eslint/recommended',
-		'plugin:prettier/recommended',
-		'plugin:react-hooks/recommended',
-	],
-	parserOptions: {
-		project: './tsconfig.json',
-	},
+	extends: ['eslint:recommended', 'plugin:prettier/recommended', 'plugin:react-hooks/recommended'],
 	plugins: ['import'],
 	rules: {
 		eqeqeq: ['error', 'always', { null: 'never' }],
 		'no-console': ['warn', { allow: ['warn', 'error'] }],
 		'sort-imports': ['error', { ignoreDeclarationSort: true, ignoreCase: true }],
-		'@typescript-eslint/explicit-module-boundary-types': [
-			'warn',
-			{ allowArgumentsExplicitlyTypedAsAny: true },
-		],
-		'@typescript-eslint/no-explicit-any': 'off',
-		'@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
-		'@typescript-eslint/strict-boolean-expressions': ['error'],
 		'import/order': [
 			'error',
 			{
@@ -42,5 +28,22 @@ module.exports = {
 			},
 		],
 	},
-	ignorePatterns: ['/build', '/*.js'],
+	overrides: [
+		{
+			files: ['*.ts?(x)'],
+			extends: ['plugin:@typescript-eslint/recommended'],
+			parserOptions: {
+				project: './tsconfig.json',
+			},
+			rules: {
+				'@typescript-eslint/explicit-module-boundary-types': [
+					'warn',
+					{ allowArgumentsExplicitlyTypedAsAny: true },
+				],
+				'@typescript-eslint/no-explicit-any': 'off',
+				'@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+				'@typescript-eslint/strict-boolean-expressions': ['error'],
+			},
+		},
+	],
 };
